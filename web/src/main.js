1e874d8ac6fe7d5d3517234df089a6197ad2d054
Avido-b <11>
2020-11-09 13:14:26 +0800

    #2 #5列表界面

--- web/src/main.js
+++ web/src/main.js
@@ -1,17 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
 import ElementUI from 'element-ui'
-import './assets/css/common.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
 
-Vue.use(ElementUI);
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-});
-
-
-createApp(App).use(store).use(router).mount('#app')
+  router: router,
+  render: h => h(App),
+}).$mount('#app')
---
+++ web/src/views/index/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="login" class="login-input">
+      <el-form>
+          <el-form-item class="username">
+              <el-input placeholder="用户名"></el-input>
+          </el-form-item>
+          <el-form-item class="petname">
+              <el-input placeholder="昵称"></el-input>
+          </el-form-item>
+          <el-form-item class="password">
+              <el-input placeholder="密码"></el-input>
+          </el-form-item>
+          <el-form-item class="confirm">
+              <el-input placeholder="确认密码"></el-input>
+          </el-form-item>
+          <el-form-item class="email">
+              <el-input placeholder="邮箱"></el-input>
+          </el-form-item>
+          <el-button>登录</el-button>
+      </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        
+    }
+}
+</script>
+
+<style> 
+.login-input{
+    width: 400px;
+}
+
+
+</style>
