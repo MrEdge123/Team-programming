ce9aec193fd4647d29fd66951dc8fb1929b76936
Avido-b <11>
2020-11-12 22:06:37 +0800

    #1

--- web/src/network/problem.js
+++ web/src/network/problem.js
@@ -1,13 +1,8 @@
 import {axios} from './request'
 
 export function getProblemMultiData(){
-<<<<<<< HEAD
     return axios({
         
-=======
-    return request({
-        url: '/getDataList/'
->>>>>>> 7aca1442c7f7f304fa04a105ed2a19e97b70490c
     })
 }
 
